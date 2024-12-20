9c040e1be8f5e3d5cc494cbac63bbe033fa1a5e2
Denisov Nikolay <Lapxi010@yandex.ru>
2024-12-20 18:48:15 +0300

    f:add

--- PlayCase/src/router/index.js
+++ PlayCase/src/router/index.js
@@ -8,7 +8,7 @@ import AdminPlaces from "@/admin/AdminPlaces.vue";
 import AdminPlace from "@/admin/AdminPlace.vue";
 
 const routes = [{
-        path: '/',
+        path: '/PlayCase',
         name: 'TeamNameInput',
         component: TeamNameInput,
     },
